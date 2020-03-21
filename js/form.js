c2a45b133d46c6db41b5a19f412e682a57e051eb
gusjara <tavit02@hotmail.com>
2020-03-21 15:54:02 -0300

    commit inicial

---
+++ js/form.js
@@ -0,0 +1,12 @@
+document.querySelector('#submit').addEventListener('click',function(){
+    
+    let cliente = document.querySelector('#cliente').value;
+    let fecha = document.querySelector('#fecha').value;
+    let hora = document.querySelector('#hora').value;
+    let doctor = document.querySelector('#doctor').value;
+    let consulta = document.querySelector('#consulta').value;
+
+    let url = "https://api.whatsapp.com/send?phone=543794345079&text=*_Consultas Medicas_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Doctor de preferencia*%0A" + doctor + "%0A*¿Cual es consulta que se desea realizar?*%0A" + consulta;
+    window.open(url);
+
+});
