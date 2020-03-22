f593cac16d6c4cce828ab5100b3affd64c0bb59f
gusjara <tavit02@hotmail.com>
2020-03-22 18:47:15 -0300

    cambios en las validaciones del formulario

--- js/form.js
+++ js/form.js
@@ -1,3 +1,66 @@
+// jQuery(document).ready(function($) {
+        $('#submit').hide();
+
+        let d = new Date();    
+        // date.setDate(date.getDate() + 1);
+        let month = '' + (d.getMonth() + 1);
+        if( month < 10 ) month = '0'+month
+        
+        let day = '' + (d.getDate()+1);
+        let dia = '' + d.getDate();
+        if( day < 10 ) day = '0' + day
+        
+        let year = d.getFullYear();
+        
+        let f_comp = [year, month, day].join('-')
+        
+        let f_hoy = [dia, month, year].join('-')
+        $('.fecha').val(f_comp)
+        // $('.fecha').val([day, month, year].join('/'))
+        // console.log([year, month, day].join('-'));
+
+        // });
+        let cliente, fecha, hora, doctor, cosulta = ''
+        if (cosulta == null) {
+            $('#submit').hide();
+        }
+
+        $('#cliente').change(function(event) {
+            cliente = $('#cliente').val()
+        })
+
+        $('#fecha').change(function(event) {
+            fecha = $('#fecha').val()
+            if (fecha < f_comp) {
+                Swal.fire(
+                  'Error en la fecha',
+                  'La fecha tiene que ser mayor a '+f_hoy,
+                  'error'
+                )
+            }
+        })
+
+        $('#hora').change(function(event) {
+            hora = $('#hora').val()
+        })
+
+        $('#doctor').change(function(event) {
+            doctor = $('#doctor').val()
+        })
+
+        $('#consulta').change(function(event) {
+            consulta = $('#consulta').val()
+            if (cliente == null && fecha == null && hora == null && doctor == null && consulta == null) {
+                console.log('sin')
+            }else {
+                console.log('completos')
+                $('#submit').show();
+                // $('#submit').attr('hide', false);
+            }
+        })
+
+
+
 document.querySelector('#submit').addEventListener('click',function(){
 //vars    
     let cliente = document.querySelector('#cliente').value;
